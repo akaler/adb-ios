4b44cadfb707d62f0223395b7586ca9bfeb2e7d9
Abhijot Kaler <akaler@uci.edu>
2021-05-02 14:12:14 -0700

    Added validation to login

--- adb-ios/LoginViewController.swift
+++ adb-ios/LoginViewController.swift
@@ -12,13 +12,22 @@ class LoginViewController: UIViewController {
 
     @IBOutlet weak var EmailField: UITextField!
     @IBOutlet weak var PasswordField: UITextField!
+    @IBOutlet weak var statusLabel: UILabel!
     override func viewDidLoad() {
         super.viewDidLoad()
-
+        statusLabel.alpha = 0
         // Do any additional setup after loading the view.
     }
     
     @IBAction func LoginButton(_ sender: Any) {
+        if validateFields(emailField: EmailField.text ?? "", passField: PasswordField.text ?? "") {
+            //HTTP requests
+        }
+        else
+        {
+            statusLabel.text = "Email or password is invalid. Try again"
+            statusLabel.alpha = 1
+        }
         
     }
     
@@ -31,5 +40,11 @@ class LoginViewController: UIViewController {
         // Pass the selected object to the new view controller.
     }
     */
-
+    func validateFields(emailField: String, passField: String) -> Bool {
+        if (emailField.isEmpty || passField.isEmpty) {
+            print("empty field")    // development
+            return false
+        }
+        return true
+    }
 }
